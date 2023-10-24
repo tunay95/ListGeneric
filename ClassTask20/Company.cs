34190367f9300131120edace1617b7e4c2616889
Ziver2023 <bd7hskl2t@code.edu.az>
2023-10-24 19:44:44 +0400

    SingleorDefault

--- ClassTask20/Company.cs
+++ ClassTask20/Company.cs
@@ -1,104 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassTask20
 {
-    class Company
+
+    internal class Company
     {
-        private int _id;
-        public int Id { get; set; }
-        public int Name { get; set; }
-        public int MyProperty { get; set; }
-
-        List<User> Users = new List<User>();
-
-
-        public User GetUser(int id)
+        public static int Id { get; set; }
+        public string Name { get; set; }
+        List<User> users = new List<User>();
+        public Company(string name)
         {
-            User user = null;
-            foreach (User item in Users)
-            {
-                if (item.Id == id)
-                {
-                    user = item;
-                }
-            }
-
-            if (user == null)
-            {
-                throw new DataNotFoundException();
-            }
-            return user;
+            Name = name;
+            Id++;
         }
-
-
-
-
-        public void GetAllUsers()
+        public void AddUser(User user)
         {
-
-            foreach (User user in Users)
-            {
-                Console.WriteLine(user);
-            }
-
+            users.Add(user);
         }
-
-
-
-        public void DeleteUser(int id)
-        {
-
-
-
-        }
-
-
-        public void UpdateUser(User user)
+        public User GetUserId(int id)
         {
             try
             {
-                User user2 = null;
-                for (int i = 0; i < Users.Count; i++)
+                foreach (User user in users)
                 {
-                    if (Users[i].Id == user.Id)
+                    if (user.Id == id)
                     {
-                        user2 = Users[i];
-                        break;
+                        return user;
                     }
-
-                    if (user2 != null)
-                    {
-                        user2.Name = user.Name;
-                        user2.Surname = user.Surname;
-                        user2.Username = user.Username;
-                        user2.Id = user.Id;
-
-                    }
-                    else
-                    {
-
-                        throw new DataNotFoundException();
-                    }
-
-
                 }
-
-
             }
             catch (DataNotFoundException ex)
             {
-
+                Console.WriteLine(ex.Message);
             }
-
+            return null;
+        }
+        public List<User> GetAllUsers()
+        {
+            return users;
         }
 
+        public void DeleteUser(int id)
+        {
+            try
+            {
+              
+
+
+
+
+                //foreach (User user in users)
+                //{
+                //    if (user.Id == id)
+                //    {
+                //        users.Remove(user);
+                //    }
+                
+            }
+            catch (DataNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
     }
+
+        public void UpdateUser(User person)
+        {
+            User newUser = null;
+            try
+            {
+                foreach (User user in users)
+                {
+                    if (user.Id == person.Id)
+                    {
+                        newUser.Name = user.Name;
+                        newUser.Surname = user.Surname;
+                        newUser.Username = user.Username;
+                        newUser.Id = user.Id;
+                    }
+                }
+            }
+            catch (DataNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
 }
+
 
    
